dac86af770af26a8d0120a983286392073525962
Stephen Grayson <fingrayson@gmail.com>
2017-02-18 22:43:54 -0500

    Update the User table and model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'sign_up' => 'users#new', :as => 'sign_up'
+  root 'users#new'
+  resources :users
 end
