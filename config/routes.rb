05328926f694bfcef2e4925d9ce25da2dd3bb417
Stephen Grayson <fingrayson@gmail.com>
2017-02-25 10:37:25 -0600

    implement advance login

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  root   'users#new'
+  root   'sessions#new'
 
   get    '/login',      to: 'sessions#new'
   post   '/login',     to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
 
-  get    '/sign_up', to: 'users#new'
+  get    '/signup', to: 'users#new'
   resources :users
 end
